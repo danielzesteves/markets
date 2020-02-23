341de61b1c25a0fe38651f1744be0a4393a10a79
Daniel Esteves <danielzesteves@gmail.com>
2020-02-23 13:08:56 -0300

    Funcionando properties

--- src/main/java/com/iol/services/IolAuthenticationService.java
+++ src/main/java/com/iol/services/IolAuthenticationService.java
@@ -5,28 +5,31 @@
  */
 package com.iol.services;
 
+import com.app.IolProperties;
 import interfaces.IAuthenticationService;
-import interfaces.IAuthenticationService;
-import interfaces.IToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 /**
  *
  * @author daniel
  */
 @Component
-public class IolAuthenticationService implements IAuthenticationService{
-   
-   public void getToken(){
-       System.out.println("Athu");
-   } 
+@EnableConfigurationProperties(IolProperties.class)
+public class IolAuthenticationService implements IAuthenticationService {
+
+    @Autowired
+    private IolProperties props;
+
+    public void getToken() {
+        System.out.println("Athu");
+    }
 
     @Override
     public boolean isAuthenticated() {
-        IolProperties props = new IolProperties();
-        System.out.println(props.toString());        
+        System.out.println(props.getUser());
+        System.out.println(props.toString());
         return true;
     }
 }
--- src/main/java/com/market/value/ValueApplication.java
+++ src/main/java/com/market/value/ValueApplication.java
@@ -1,6 +1,6 @@
 package com.market.value;
 
-import com.iol.services.IolProperties;
+import com.app.IolProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.controllers","com.iol.services","interfaces"})
-@EnableConfigurationProperties(IolProperties.class)
 public class ValueApplication {
 
 	public static void main(String[] args) {
