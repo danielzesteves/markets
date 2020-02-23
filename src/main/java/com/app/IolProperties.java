6c136c8cf700cf9c403d659cedeaf30d4e661664
Daniel Esteves <danielzesteves@gmail.com>
2020-02-23 13:04:11 -0300

    Properties funcionando

--- src/main/java/com/app/IolProperties.java
+++ src/main/java/com/app/IolProperties.java
@@ -3,20 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.iol.services;
+package com.app;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  *
- * @author desteves
+ * @author daniel
  */
-
-@ConfigurationProperties(prefix="iol")
+@ConfigurationProperties(prefix = "iol")
 @Data
 public class IolProperties {
+    private String pass;
     private String user;
-    private String pass;  
 }
